1738dce16e802e767e8b4a94e11f46ee9e1564d7
EzeVedder <pane.ezequiel@gmail.com>
2019-09-19 11:52:59 -0300

    ejercicio con objetos en js

---
+++ ejercitacion/#OBJETOS/js/objetos.js
@@ -0,0 +1,52 @@
+
+
+
+var Banco = {
+    usuarios: [],
+
+    crearUsuario: function(p_nombre,p_saldo) {
+        var usuario = {
+            nombre: p_nombre,
+            saldo: p_saldo
+        }
+        return  this.usuarios.push(usuario);
+    },
+
+    // método para consultar saldo de usuario pasado por parametro.
+    consultarSaldo: function(p_usuario) {
+        debugger
+        for(var i=0; i<this.usuarios.length; i++) {
+            var usu = this.usuarios[i];
+            if(usu.nombre === p_usuario) {
+                return usu.saldo;
+            }else{
+                console.log('Usuario no encontrado.');
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// funcion para crear objeto persona
+
+function crearPersona(p_nombre,p_saldo) {
+    var persona = {
+        nombre: p_nombre,
+        billetera: p_saldo,
+        enriquecer: function(monto) {
+            this.billetera = this.billetera+monto;
+            return this.billetera;
+        }
+    }
+    return persona;
+}
