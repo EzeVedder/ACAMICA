2f2d1d7dff18384de3870f6b9448a265a8d7e639
EzeVedder <pane.ezequiel@gmail.com>
2019-09-23 09:57:11 -0300

    funcionalidad pintar grilla terminada.

--- TPS/PaneEzequiel-PixelArt/js/pixel-art.js
+++ TPS/PaneEzequiel-PixelArt/js/pixel-art.js
@@ -1,4 +1,10 @@
 
+var paletaDeColores = [];
+var grillaDeColores = 1749;
+var colorElegido;
+var colorRGB;
+
+
 $(document).ready(function(){
   console.log("JQuery cargado...");
 var nombreColores = ['White', 'LightYellow',
@@ -23,7 +29,163 @@ var nombreColores = ['White', 'LightYellow',
   'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
 ];
 
-var paletaDeColores = [];
+function nameToHex(name) {
+  return {
+    "aliceblue": "#f0f8ff",
+    "antiquewhite": "#faebd7",
+    "aqua": "#00ffff",
+    "aquamarine": "#7fffd4",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "bisque": "#ffe4c4",
+    "black": "#000000",
+    "blanchedalmond": "#ffebcd",
+    "blue": "#0000ff",
+    "blueviolet": "#8a2be2",
+    "brown": "#a52a2a",
+    "burlywood": "#deb887",
+    "cadetblue": "#5f9ea0",
+    "chartreuse": "#7fff00",
+    "chocolate": "#d2691e",
+    "coral": "#ff7f50",
+    "cornflowerblue": "#6495ed",
+    "cornsilk": "#fff8dc",
+    "crimson": "#dc143c",
+    "cyan": "#00ffff",
+    "darkblue": "#00008b",
+    "darkcyan": "#008b8b",
+    "darkgoldenrod": "#b8860b",
+    "darkgray": "#a9a9a9",
+    "darkgreen": "#006400",
+    "darkkhaki": "#bdb76b",
+    "darkmagenta": "#8b008b",
+    "darkolivegreen": "#556b2f",
+    "darkorange": "#ff8c00",
+    "darkorchid": "#9932cc",
+    "darkred": "#8b0000",
+    "darksalmon": "#e9967a",
+    "darkseagreen": "#8fbc8f",
+    "darkslateblue": "#483d8b",
+    "darkslategray": "#2f4f4f",
+    "darkturquoise": "#00ced1",
+    "darkviolet": "#9400d3",
+    "deeppink": "#ff1493",
+    "deepskyblue": "#00bfff",
+    "dimgray": "#696969",
+    "dodgerblue": "#1e90ff",
+    "firebrick": "#b22222",
+    "floralwhite": "#fffaf0",
+    "forestgreen": "#228b22",
+    "fuchsia": "#ff00ff",
+    "gainsboro": "#dcdcdc",
+    "ghostwhite": "#f8f8ff",
+    "gold": "#ffd700",
+    "goldenrod": "#daa520",
+    "gray": "#808080",
+    "green": "#008000",
+    "greenyellow": "#adff2f",
+    "honeydew": "#f0fff0",
+    "hotpink": "#ff69b4",
+    "indianred ": "#cd5c5c",
+    "indigo": "#4b0082",
+    "ivory": "#fffff0",
+    "khaki": "#f0e68c",
+    "lavender": "#e6e6fa",
+    "lavenderblush": "#fff0f5",
+    "lawngreen": "#7cfc00",
+    "lemonchiffon": "#fffacd",
+    "lightblue": "#add8e6",
+    "lightcoral": "#f08080",
+    "lightcyan": "#e0ffff",
+    "lightgoldenrodyellow": "#fafad2",
+    "lightgrey": "#d3d3d3",
+    "lightgreen": "#90ee90",
+    "lightpink": "#ffb6c1",
+    "lightsalmon": "#ffa07a",
+    "lightseagreen": "#20b2aa",
+    "lightskyblue": "#87cefa",
+    "lightslategray": "#778899",
+    "lightsteelblue": "#b0c4de",
+    "lightyellow": "#ffffe0",
+    "lime": "#00ff00",
+    "limegreen": "#32cd32",
+    "linen": "#faf0e6",
+    "magenta": "#ff00ff",
+    "maroon": "#800000",
+    "mediumaquamarine": "#66cdaa",
+    "mediumblue": "#0000cd",
+    "mediumorchid": "#ba55d3",
+    "mediumpurple": "#9370d8",
+    "mediumseagreen": "#3cb371",
+    "mediumslateblue": "#7b68ee",
+    "mediumspringgreen": "#00fa9a",
+    "mediumturquoise": "#48d1cc",
+    "mediumvioletred": "#c71585",
+    "midnightblue": "#191970",
+    "mintcream": "#f5fffa",
+    "mistyrose": "#ffe4e1",
+    "moccasin": "#ffe4b5",
+    "navajowhite": "#ffdead",
+    "navy": "#000080",
+    "oldlace": "#fdf5e6",
+    "olive": "#808000",
+    "olivedrab": "#6b8e23",
+    "orange": "#ffa500",
+    "orangered": "#ff4500",
+    "orchid": "#da70d6",
+    "palegoldenrod": "#eee8aa",
+    "palegreen": "#98fb98",
+    "paleturquoise": "#afeeee",
+    "palevioletred": "#d87093",
+    "papayawhip": "#ffefd5",
+    "peachpuff": "#ffdab9",
+    "peru": "#cd853f",
+    "pink": "#ffc0cb",
+    "plum": "#dda0dd",
+    "powderblue": "#b0e0e6",
+    "purple": "#800080",
+    "red": "#ff0000",
+    "rosybrown": "#bc8f8f",
+    "royalblue": "#4169e1",
+    "saddlebrown": "#8b4513",
+    "salmon": "#fa8072",
+    "sandybrown": "#f4a460",
+    "seagreen": "#2e8b57",
+    "seashell": "#fff5ee",
+    "sienna": "#a0522d",
+    "silver": "#c0c0c0",
+    "skyblue": "#87ceeb",
+    "slateblue": "#6a5acd",
+    "slategray": "#708090",
+    "snow": "#fffafa",
+    "springgreen": "#00ff7f",
+    "steelblue": "#4682b4",
+    "tan": "#d2b48c",
+    "teal": "#008080",
+    "thistle": "#d8bfd8",
+    "tomato": "#ff6347",
+    "turquoise": "#40e0d0",
+    "violet": "#ee82ee",
+    "wheat": "#f5deb3",
+    "white": "#ffffff",
+    "whitesmoke": "#f5f5f5",
+    "yellow": "#ffff00",
+    "yellowgreen": "#9acd32"
+  }[name.toLowerCase()];
+}
+
+/////////
+function hex2rgb(c) {
+  if (c[0] === '#') c = c.substr(1);
+  var r = parseInt(c.slice(0,2), 16),
+      g = parseInt(c.slice(2,4), 16),
+      b = parseInt(c.slice(4,6), 16);
+  return 'rgb('+ r +','+ g +','+ b +')';
+}
+
+
+
+
 
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
@@ -32,15 +194,13 @@ var colorPersonalizado = document.getElementById('color-personalizado');
 colorPersonalizado.addEventListener('change', 
   (function() {
     // Se guarda el color de la rueda en colorActual
+    //debugger
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
 
 
   })
 );
-
-
-
 
 
 /**
@@ -50,7 +210,7 @@ colorPersonalizado.addEventListener('change',
  function cargarPaletaDeColores(p_arrayColores) {
    
     var $paletaColores = $("#paleta");
-    debugger
+    //debugger
     for(var i=0; i<p_arrayColores.length; i++){
       var colorActual = p_arrayColores[i];
       $paletaColores.append("<div></div>");
@@ -59,14 +219,58 @@ colorPersonalizado.addEventListener('change',
       $div_actual.attr("class","color-paleta");
       $div_actual.css({"background-color":colorActual});
       //$paletaColores.children("div").css({"background-color":colorActual});
-
+      paletaDeColores.push($div_actual);
+      $div_actual.click(getColor);
 
     }
 
  }
 
+
+/**
+ * FUNCION PARA GENERAR LA GRILLA DE COLORES DE MANERA DINÁMICA
+ */
+
+ function cargarGrillaDeColores(p_grillaDeColores) {
+  var $grillaDeColores = $("#grilla-pixeles");
+  //debugger
+  for(var i=0; i<p_grillaDeColores; i++){
+    $grillaDeColores.append("<div></div>");
+    var $div_actual = $grillaDeColores.children().eq(i);
+    $div_actual.attr("id",i);
+    $div_actual.click(pintarGrilla);
+  }
+}
+
+/** FUNCION PARA MODIFICAR EL INDICADOR DE COLOR */
+
+function getColor(event) {
+  //debugger
+  colorElegido = this.id;
+  var indicadorDeColor = $("#indicador-de-color");
+  indicadorDeColor.css("background-color",colorElegido);
+  var colorHex = nameToHex(colorElegido);
+  colorRGB = hex2rgb(colorHex);
+  agregarMensajeDeColor(colorRGB);
+}
+
+function agregarMensajeDeColor(p_colorElegido){
+  $("#indicador-de-color-mensaje").html("<p>PINCEL "+p_colorElegido+"</p>");
+}
+
+function pintarGrilla(event) {
+  //debugger
+  //var $elemento_pixel = $("#grilla-pixeles").children(this); SELECCIONO TODA LA GRILLA
+  var $elemento_pixel = $("#grilla-pixeles").children().eq(this.id);
+  $elemento_pixel.css("background-color",colorRGB);
+  //this.id.style.background = colorElegido;
+
+}
+
+
+/** INICIO PROGRAMA */
+
  cargarPaletaDeColores(nombreColores);
-
-
+ cargarGrillaDeColores(grillaDeColores);
 
 });
